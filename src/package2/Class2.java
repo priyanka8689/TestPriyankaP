6d37d4e776d5583b0e8d0259ca21ef51a44432ff
Priyanka Panat <priyankapanat30@gmail.com>
2021-07-13 08:33:49 -0700

    Added branch B1 changes

--- src/package2/Class2.java
+++ src/package2/Class2.java
@@ -3,5 +3,7 @@ package package2;
 public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("Hi_123");
+		System.out.println("In B1");
+		System.out.println("checking conflict sce");
 	}
 }
