fc19fa24dcc998773132f2c0b2ef6c83a3a0b495
Priyanka Panat <priyankapanat30@gmail.com>
2021-07-13 08:24:31 -0700

    BranchBmsg

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,8 +2,14 @@ package package1;
 
 public class Class1 {
 	public static void main(String[] args) {
+
 		System.out.println("Hi hello");
 		System.out.println("In branch A");
 		System.out.println("Hey there");
+
+		System.out.println("Hi sd");
+		System.out.println("In branch B hsj");
+		System.out.println("I am in branch B sjd");
+
 	}
 }
