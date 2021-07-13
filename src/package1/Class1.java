1f2059e6a6872ea28064e5496a04f3d01a752ce8
Priyanka Panat <priyankapanat30@gmail.com>
2021-07-13 08:03:37 -0700

    Changes in A

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,6 +2,8 @@ package package1;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Hi hello");
+		System.out.println("In branch A");
+		System.out.println("Hey there");
 	}
 }
